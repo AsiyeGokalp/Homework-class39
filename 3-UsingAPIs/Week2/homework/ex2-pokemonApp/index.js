b76f0bdcc7be166f2f6804171f450bc49fd31726
asiyeGokalp <gokalpasiye@gmail.com>
2022-09-21 01:08:11 +0200

    hw done

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
 
@@ -21,19 +22,67 @@ Use async/await and try/catch to handle promises.
 
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
+In this exercise you're going to do several things:
+
+1. Create and append DOM elements using JavaScript only.
+2. Fetch data from a public API: <https://pokeapi.co/api/v2/pokemon?limit=151>
+3. Display the results in the DOM.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('response was not okay');
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchAndPopulatePokemons(url, select) {
+  try {
+    const data = await fetchData(url);
+    for (const name of data['results']) {
+      const option = document.createElement('option');
+      option.textContent = name.name;
+      select.appendChild(option);
+    }
+  } catch (err) {
+    console.log('Error:' + err);
+  }
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(url, img) {
+  try {
+    const response = await fetch(url);
+    const data = await response.blob();
+    img.src = URL.createObjectURL(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function main() {
-  // TODO complete this function
+  const URL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
+  const button = document.createElement('button');
+  const img = document.createElement('img');
+  const select = document.createElement('select');
+
+  button.textContent = 'Get pokemon!';
+  button.getAttribute('type', 'submit');
+  img.getAttribute('src', '');
+  img.getAttribute('alt', 'pokemon');
+
+  document.body.appendChild(button);
+  document.body.appendChild(img);
+  document.body.appendChild(select);
+
+  button.addEventListener('click', () => {
+    fetchAndPopulatePokemons(URL, select);
+  });
+
+  select.addEventListener('change', () => {
+    fetchImage(URL, img);
+  });
 }
+
+window.addEventListener('load', main);
